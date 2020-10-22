7bb653c220575bbe0581ce4b37744fdf39f8c816
Farinhote <andrefarinhote@gmail.com>
2020-10-22 23:16:54 +0100

    Add zebra listing
    Add more global sass variables
    Center home thru use of a mixin

--- q4/client/book-client/src/components/ListBooks.vue
+++ q4/client/book-client/src/components/ListBooks.vue
@@ -1,14 +1,19 @@
 <template>
   <fragment>
     <h1>Top books of all time</h1>
-    <Book v-for="book in books" :key="book.slug" :book="book" />
+    <div class="book-wrapper">  <!-- Hacky workaround for v-deep -->
+      <Book
+        v-for="(book, index) in books"
+        :key="book.slug"
+        :book="{ ...book, position: index + 1 }"
+      />
+    </div>
   </fragment>
 </template>
 
 <script>
-
-import { Fragment } from 'vue-fragment';
-import Book from '@/components/Book.vue';
+import { Fragment } from "vue-fragment";
+import Book from "@/components/Book.vue";
 
 export default {
   name: "ListBooks",
@@ -19,13 +24,16 @@ export default {
   },
 
   computed: {
-    books () {
-      return this.$store.state.books
-    }
-  }
+    books() {
+      return this.$store.state.books;
+    },
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+.book-wrapper ::v-deep .book:nth-child(odd) {
+  background-color: $secondary-background;
+}
 </style>
